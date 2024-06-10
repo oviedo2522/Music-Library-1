7b055139f801f60035b2841c7dd91247d16fd988
Daniel Fernandez <mdanif9728@gmail.com>
2024-06-10 01:19:10 -0700

    add style to the forms

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -8,8 +8,10 @@ function SearchBar(props) {
       <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
 
         <input type='text' placeholder='Enter a search term here'
-          onChange={(e) => setSearchTerm(e.target.value)} />
-
+          onChange={
+            (e) => props.handleSearch(e, e.target.value)}
+            // (e) => setSearchTerm(e.target.value)} // Work with the button.
+        />
         <input type="submit" />
       </form>
     </div>
