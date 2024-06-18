90955637979a1f4cb6ecca25ff46762414510950
oviedo2522 <oviedo2522@gmail.com>
2024-06-17 19:49:56 -0700

    This version works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import Gallery from './Gallery'
+import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 
 function App() {
